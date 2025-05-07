faecec6a38e5e8be05ec052e7bbd2547ee7e8382
Faded <granveer308@gmail.com>
2025-05-07 18:10:25 +1200

    Update solver.py

--- solver.py
+++ solver.py
@@ -7,7 +7,7 @@ client = genai.Client(api_key="api")
 #this works but is NOT accurate i made this because i felt like it lolll
 def solve(path):
     image = Image.open(path)
-    prompt = ("You are a precise OCR assistant. Extract and return only the exact characters shown in this CAPTCHA image, without any extra formatting or commentary.")
+    prompt = ("You are a OCR assistant. Extract and return only the exact characters shown in this CAPTCHA image, without any extra formatting or commentary.")
     start = time.time()
     response = client.models.generate_content(model="gemini-2.0-flash", contents=[image, prompt])
     elapsed = time.time() - start
