37a4f0bb5ff1c82d715cbb2f860af4a9876e8e09
Faded <granveer308@gmail.com>
2025-08-24 18:32:35 +1200

    Update solver.py

--- solver.py
+++ solver.py
@@ -1,17 +1,18 @@
 import time
 from PIL import Image
-from google import genai #gpt didnt work for me some reason idk
+import google.generativeai as genai #gpt didnt work for me some reason idk
 
-client = genai.Client(api_key="api")
+genai.configure(api_key="AIzaSyAE7H4JuCJnySo8jCck_LTlN0k3gOQkfCU")
 
 #this works but is NOT accurate i made this because i felt like it lolll
 def solve(path):
     image = Image.open(path)
-    prompt = ("You are a OCR assistant. Extract and return only the exact characters shown in this CAPTCHA image, without any extra formatting or commentary.")
+    prompt = ("You are an OCR assistant. Extract and return ONLY the exact characters shown in this CAPTCHA image.")
     start = time.time()
-    response = client.models.generate_content(model="gemini-2.0-flash", contents=[image, prompt])
+    model = genai.GenerativeModel("gemini-2.0-flash")
+    response = model.generate_content([prompt, image])
     elapsed = time.time() - start
     return response.text.strip(), elapsed
 
 text, duration = solve("captcha.png")
-print(text, f"- {duration:.2f}")
+print(text, f"- {duration:.2f}s")
